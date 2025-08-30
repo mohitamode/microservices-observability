ba9490b0abc2903dfec42815ea8f91e2bb3ac396
Mohit Shyam Amode <mohitamode@gmail.com>
2025-08-30 13:42:00 +0000

    feat(api): extend service capabilities

---
+++ reactive-calendar-api/src/main/java/com/microservices/calendar/api/controller/Errors.java
@@ -0,0 +1,24 @@
+package com.microservices.calendar.api.controller;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.RoutingContext;
+
+public class Errors {
+
+    public static void error(RoutingContext ctx, int status, String cause) {
+        JsonObject error = new JsonObject()
+            .put("error", cause)
+            .put("code", status)
+            .put("path", ctx.request().path());
+        ctx.response()
+            .putHeader("Content-Type", "application/json")
+            .setStatusCode(status)
+            .end(error.encodePrettily());
+    }
+
+    public static void error(RoutingContext ctx, int status, Throwable cause) {
+        error(ctx, status, cause.getMessage());
+    }
+
+
+}
