500d213035214a2d3e544e9c71dabbd3a82b5038
Mohit Shyam Amode <mohitamode@gmail.com>
2025-08-30 13:42:00 +0000

    feat(api): extend service capabilities

---
+++ calendar-integration/src/main/java/com/microservices/calendar/fuse/CalendarFuseApplication.java
@@ -0,0 +1,23 @@
+package com.microservices.calendar.fuse;
+
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@CamelOpenTracing
+public class CalendarFuseApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(CalendarFuseApplication.class);
+
+    @Value("${camel.component.servlet.mapping.contextPath}")
+    private String contextPath;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarFuseApplication.class, args);
+    }
+
+}
